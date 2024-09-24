11e679262d83e5384c1dd067efeedfbaa8dc0408
Yinus Alamutu <Yalamutu2169@conestogac.on.ca>
2024-09-23 20:43:34 -0400

    Initial source code commit and push with inclusion of other files

---
+++ Assignment 2/Source.cpp
@@ -0,0 +1,44 @@
+//Yinus Adeyinka Alamutu - Project V - Assignment 2
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct STUDENT_DATA //struct that contains student data
+{
+    std::string firstName;
+    std::string lastName;
+};
+
+int main(void)
+{
+    ifstream inputFile("C:\\Users\\adeyi\\source\\repos\\ProjectV\\StudentData.txt");
+    //Vector holding students
+    vector<STUDENT_DATA> students;
+    //if there was an error accessing the input file, users will be notified
+    if (!inputFile) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(inputFile, line)) {
+        stringstream ss(line);
+        string firstName, lastName; // string holding firstname and lastname
+
+        // Splitting the line by the comma
+        if (getline(ss, lastName, ',') && getline(ss, firstName))
+        {
+            // Creating a STUDENT_DATA object and pushing it into the vector
+            STUDENT_DATA student;
+            student.firstName = firstName;
+            student.lastName = lastName;
+            students.push_back(student);
+        }
+    }
+
+    inputFile.close();
+    return 0;
+}
